c590f97371406c49170182bd2d96366e4db865fd
Mikail Khan <mikail.khan45@gmail.com>
2019-10-29 23:58:37 -0400

    unomegadumbed isprime

--- rsa/isprime.cpp
+++ rsa/isprime.cpp
@@ -1,30 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <iostream>
-#include <vector>
-#include <algorithm>
 #include <math.h>
 
 using namespace std;
 
-bool isPrime(unsigned long long n, vector<int> &primes) {
+bool isPrime(unsigned long long n) {
+   if (n % 2 == 0) {
+      return false;
+   }
    for(int i = 3; i < sqrt((double) n)  + 1; i+=2) {
-      bool in_list = find(primes.begin(), primes.end(), i) != primes.end();
-      if (in_list || isPrime(i, primes)) {
-         if (!in_list)
-            primes.push_back(i);
-         if (n % i == 0) {
-            return false;
-         }
+      if (n % i == 0) {
+         return false;
       }
    }
    return true;
-}
-
-bool isPrime(unsigned long long n) {
-   vector<int> primes = {2, 3, 5};
-   cout << n << "\n";
-   return isPrime(n, primes);
 }
 
 int main(int argc, char *argv[]) {
--- rsa/isprime.rs
+++ rsa/isprime.rs
@@ -1,20 +1,16 @@
 use std::env;
-use std::cell::RefCell;
 
 fn main() {
-    println!("{}", isPrime(env::args().nth(1).unwrap().parse().unwrap(), &[]));
+    println!("{}", is_prime(env::args().nth(1).unwrap().parse().unwrap()));
 }
 
-fn isPrime(n: usize, primes: &[usize]) -> bool {
-    let prime_vec: RefCell<Vec<usize>> = RefCell::new(primes.into());
-    if prime_vec.borrow().len() < 3 {
-        *prime_vec.borrow_mut() = vec![2, 3, 5];
+fn is_prime(n: usize) -> bool {
+    let sqrt_ish = (n as f32).sqrt() as usize + 1;
+    if n % 2 == 0 {
+        false
+    } else {
+        (3..sqrt_ish).step_by(2).all(|i|{
+                n % i != 0
+        })
     }
-    
-    (3..(n as f32).sqrt() as usize + 1).step_by(2).filter(|n| prime_vec.borrow().contains(n) || isPrime(*n, &prime_vec.borrow())).all(|i|{
-        if !prime_vec.borrow().contains(&i) {
-            prime_vec.borrow_mut().push(i);
-        }
-        n % i != 0
-    })
 }
